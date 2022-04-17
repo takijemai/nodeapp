0a10fca6c40cefab91347ef855685299bf0e60b1
JEMAI TAKIEDDINE <jt800@inlumine.ual.es>
2022-04-17 23:19:33 +0200

    up

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const HelloWordService = require( "./services/hello-world" );
+const HelloWordService = require('./services/hello-world')
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/:nameToSalute', (req, res) => {
-  res.send(new HelloWordService().greet(req.params.nameToSalute));
+    res.send(new HelloWordService().greet(req.params.nameToSalute))
 })
 
 module.exports = app
